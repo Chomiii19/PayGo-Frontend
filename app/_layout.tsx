d038805bfeeb07e5942bb22d7cd62f5c1c564418
Chomiii19 <jomariborines55@gmail.com>
2025-03-13 20:51:42 +0800

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      {" "}
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
